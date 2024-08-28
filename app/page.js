837736d506f5fe98e035e2e512a6a926cfececb6
Anushka Verma <130084364+AnuV1905@users.noreply.github.com>
2024-08-28 12:43:48 +0000

    added buttons

--- app/page.js
+++ app/page.js
@@ -1,8 +1,29 @@
-import Image from "next/image";
+import Image from 'next/image'
+import getStripe from '@/utils/get-stripe'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { Button } from '@mui/material'
 
 
 export default function Home() {
-  return (
-    
+    <Container maxWidth="lg">
+      <Head>
+        <title>FLashcard Saas</title>
+        <meta name="description" content="Create flash from your text" />
+      </Head>
+      
+      <Appbar position="static">
+        <Toolbar>
+          <Typography variant = "h6">Flashcard Saas</Typography>
+          <SignedOut>
+            <Button>Login</Button>
+            <Button>Sing Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </Appbar>
+    </Container>
+      
   )
 }
